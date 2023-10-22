9bfb8450d10aef0abb7f8f346ce9096b3f7e4686
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2023-10-22 20:17:24 -0300

    Added routes

--- routes/mqtt.js
+++ routes/mqtt.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const mqttController = require('../controllers/mqttController');
 
 router.post('/mqttEndpoint', mqttController.mqttEndpoint);
+router.post('/publish', mqttController.publishMessage);
+router.post('/subscribe', mqttController.subscribeTopic);
+
 
 module.exports = router;
