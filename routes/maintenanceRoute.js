f19425badade6b56fd9788a664b775dc67938642
gonzamato <gonzalo.mato@ing.austral.edu.ar>
2023-11-08 10:30:45 -0300

    added routes

--- routes/maintenanceRoute.js
+++ routes/maintenanceRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const maintenanceController = require('../controllers/maintenanceController');
+const adminController = require('../controllers/adminController');
 import {authenticateJWT} from "../JWT/protectRoutes";
 
 router.use(authenticateJWT("maintenance")); //MiddleWare
@@ -8,6 +9,9 @@ router.use(authenticateJWT("maintenance")); //MiddleWare
 router.put('/refillMachine/:machineId' , maintenanceController.refillMachine);
 router.put('/workingInMachine/:machineId' , maintenanceController.workingInMachine);
 router.put('/machineReady/:machineId' , maintenanceController.machineReady);
-
+router.get('/getMachine/:machineId' , adminController.getMachine);
+router.get('/getAllMachines' , adminController.getAllMachines);
+router.get('/getProduct/:productId' , adminController.getProduct);
+router.get('/getAllProducts' , adminController.getAllProducts);
 
 module.exports = router;
