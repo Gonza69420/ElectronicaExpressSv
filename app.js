b7b6b8367604547b6618f274c32e6ef623a4c533
Ubuntu <ubuntu@ip-172-31-40-239.ec2.internal>
2023-11-09 20:25:13 +0000

    fix user created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const config = require('./.config.js');
+const bcrypt = require('bcrypt');
 
 // Connect to MongoDB
 mongoose.connect(config.mongodbURL, {
@@ -30,5 +31,41 @@ app.use('/admin', adminRoutes);
 app.use('/maintenance', maintenanceRoutes);
 app.use('/user', userRoute);
 
+app.post('/createAdmin', async (req, res) => {
+    try {
+        const adminRole = await Role.findOne({ name: 'Admin' });
+
+        if (!adminRole) {
+            // If the "Admin" role does not exist, you can create it first
+            const newAdminRole = new Role({ name: 'Admin' });
+            await newAdminRole.save();
+        }
+
+	    const username = "admin";
+	    const name= "Senor Admin";
+        // Now, you can create a user with the "Admin" role
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash('admin123', salt, async (err, hash) => {
+                if (err) {
+                    res.status(500).json({message: 'Error creating the user', err});
+                } else {
+                    const user = new User({
+                        username,
+                        password: hash,
+                        name,
+                        role: adminRole,
+                    });
+
+                    await user.save();
+                }
+            })
+        })
+
+        res.status(201).json("");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating user.');
+    }
+})
 
 module.exports = app;
