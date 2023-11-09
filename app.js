d6fb8c18b5df483037cbc87605b335f28a82de6e
gonzamato <gonzalo.mato@ing.austral.edu.ar>
2023-11-09 15:05:26 -0300

    added create admin test

--- app.js
+++ app.js
@@ -24,9 +24,38 @@ app.use(bodyParser.json());
 const adminRoutes = require('./routes/adminRoute.js');
 const maintenanceRoutes = require('./routes/maintenanceRoute');
 const userRoute = require('./routes/userRoute');
+const {Role, User} = require("./models/user");
 
 app.use('/admin', adminRoutes);
 app.use('/maintenance', maintenanceRoutes);
 app.use('/user', userRoute);
+
+app.post('/createAdmin', async (req, res) => {
+    try {
+        const adminRole = await Role.findOne({ name: 'Admin' });
+
+        if (!adminRole) {
+            // If the "Admin" role does not exist, you can create it first
+            const newAdminRole = new Role({ name: 'Admin' });
+            await newAdminRole.save();
+        }
+
+        // Now, you can create a user with the "Admin" role
+        const adminUser = new User({
+            username: 'admin',
+            password: 'admin123',
+            name: 'Admin User',
+            role: adminRole,
+        });
+
+        await adminUser.save();
+
+        res.status(201).json(adminUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating user.');
+    }
+})
+
 
 module.exports = app;
